fa0c041e5fd18f93aaa456ef6c7f4640c1d437ab
keven silva <keven.almeida.8179@gmail.com>
2023-09-10 21:21:29 -0300

    adicionando scopo inicial

--- EvenPyApp/models.py
+++ EvenPyApp/models.py
@@ -1,44 +1,60 @@
 from django.db import models
 from datetime import date
 
+
 class BasicModel(models.Model):
     name_f = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name_f
 
+
 # All users who have a login
 class AbstractUser(BasicModel):
-    username_f    = models.CharField(max_length=100)
-    email_f       = models.CharField(max_length=100)
-    password_f    = models.CharField(max_length=20)
+    username_f = models.CharField(max_length=100)
+    email_f = models.CharField(max_length=100)
+    password_f = models.CharField(max_length=20)
+
     def __str__(self):
         return self.username_f
+
 
 # When joined in an event the user becomes a participant in that event
 class Participant(AbstractUser):
     pass
 
+
 # The Instructor is a participant responsible for some activitiy
 class Instructor(Participant):
     pass
 
-# The organizer is the participant who created the event and when interacting with 
+
+# The organizer is the participant who created the event and when interacting with
 # chosen event he will see it from the Organizer's perspective
 class Organizer(Participant):
     pass
 
+
 class Location(BasicModel):
     pass
 
+
 class Event(BasicModel):
-    organizer_f   = models.ForeignKey(Organizer, on_delete=models.CASCADE)
-    start_date_f  = models.DateTimeField()
-    end_date_f    = models.DateTimeField()
+    organizer_f = models.ForeignKey(Organizer, on_delete=models.CASCADE)
+    start_date_f = models.DateTimeField()
+    end_date_f = models.DateTimeField()
+
 
 class Activity(BasicModel):
-    start_hour_f      = models.DateTimeField()
-    end_hour_f        = models.DateTimeField()
-    location_f        = models.ForeignKey(Location, related_name='location', on_delete=models.CASCADE)
-    instructor_f      = models.ForeignKey(Instructor, related_name='instructor', on_delete=models.CASCADE)
-    event_f           = models.ForeignKey(Event, related_name='event', on_delete=models.CASCADE)
-    participants_f    = models.ManyToManyField(Participant, related_name='participant', blank=True)
+    start_hour_f = models.DateTimeField()
+    end_hour_f = models.DateTimeField()
+    location_f = models.ForeignKey(
+        Location, related_name="location", on_delete=models.CASCADE
+    )
+    instructor_f = models.ForeignKey(
+        Instructor, related_name="instructor", on_delete=models.CASCADE
+    )
+    event_f = models.ForeignKey(Event, related_name="event", on_delete=models.CASCADE)
+    participants_f = models.ManyToManyField(
+        Participant, related_name="participant", blank=True
+    )
--- EvenPyApp/views.py
+++ EvenPyApp/views.py
@@ -5,11 +5,16 @@ from .models import Participant
 def login(request):
     latest_login_users = Participant.objects.order_by("-username_f")
     context = { "latest_login_users": latest_login_users }
-    return render(request, "EvenPyApp/login.html", context)
+    return render(request, "EvenPyApp/pages/login.html", context)
 
 def register(request):
-    return HttpResponse("Register screen")
+    return render(request, "EvenPyApp/pages/register.html")
+
+
+def index(request):
+    return render(request, "EvenPyApp/pages/index.html")
+
 
 def main_page(request, user_id):
     participant = get_object_or_404(Participant, pk=user_id)
-    return render(request, "EvenPyApp/main_page.html", {"participant":participant})
+    return render(request, "EvenPyApp/pages/main_page.html", {"participant":participant})
